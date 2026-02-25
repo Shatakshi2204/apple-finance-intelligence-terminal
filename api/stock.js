933a5e19dcb8c3a142b176581cdc24007d24e2dd
Shatakshi Guha <shatakshiguha2022@vitbhopal.ac.in>
2026-02-25 10:56:50 +0530

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -1,24 +1,40 @@
 export default async function handler(req, res) {
 
+const API_KEY = "d6etmj1r01qvn4o11jp0d6etmj1r01qvn4o11jpg";
+
 try {
 
-const API_KEY = "d6etmj1r01qvn4o11jp0d6etmj1r01qvn4o11jpg";
+// CURRENT DATA
 
 const quote = await fetch(
 `https://finnhub.io/api/v1/quote?symbol=AAPL&token=${API_KEY}`
 );
 
-const metric = await fetch(
-`https://finnhub.io/api/v1/stock/metric?symbol=AAPL&metric=all&token=${API_KEY}`
-);
-
-const profile = await fetch(
-`https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=${API_KEY}`
-);
-
 const quoteData = await quote.json();
-const metricData = await metric.json();
-const profileData = await profile.json();
+
+
+// HISTORICAL DATA
+
+const today = Math.floor(Date.now()/1000);
+
+const past = today - (60*60*24*30);
+
+const history = await fetch(
+`https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=D&from=${past}&to=${today}&token=${API_KEY}`
+);
+
+const historyData = await history.json();
+
+
+const chart = historyData.c.map((price,index)=>({
+
+price:price,
+
+time:index
+
+}));
+
+
 
 res.status(200).json({
 
@@ -26,25 +42,17 @@ price: quoteData.c,
 
 change: quoteData.dp,
 
-volume: quoteData.v,
-
-marketCap: profileData.marketCapitalization * 1000000,
-
-high52: metricData.metric["52WeekHigh"],
-
-low52: metricData.metric["52WeekLow"],
-
-previousClose: quoteData.pc
+chart: chart
 
 });
 
 }
 
-catch {
+catch{
 
 res.status(200).json({
 
-price: null
+price:null
 
 });
 
